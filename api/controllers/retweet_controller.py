c7d4e45bbb9fd5460a328363b1633b4b2121137d
jaygala <jaygala260@gmail.com>
2023-03-17 17:07:55 +0530

    added code for retweet.py

--- api/controllers/retweet_controller.py
+++ api/controllers/retweet_controller.py
@@ -1,1 +1,27 @@
 # Controller for handling retweets
+from utils.twitter import get_api
+import tweepy
+import time
+
+def retweet_news():
+    api = get_api()
+
+    # search for tweets with the hashtag #cybersecuritynews
+    query = '#cybersecuritynews'
+    tweets = api.search(q=query, lang='en', result_type='recent', count=10)
+
+    # loop through the tweets and retweet them
+    for tweet in tweets:
+        if not tweet.retweeted:
+            try:
+                api.retweet(tweet.id)
+                print(f"Retweeted tweet with id {tweet.id}")
+                time.sleep(5)
+            except tweepy.TweepError as e:
+                print(f"Error retweeting tweet with id {tweet.id}: {e}")
+
+set_hours = 24
+
+while True:
+    retweet_news()
+    time.sleep(set_hours * 60 * 60)
