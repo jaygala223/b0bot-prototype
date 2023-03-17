141d89fcf30edb03cd493e3cd27570e4b9910161
jaygala <jaygala260@gmail.com>
2023-03-17 17:35:50 +0530

    update code for reply_controller

--- api/controllers/reply_controller.py
+++ api/controllers/reply_controller.py
@@ -1,15 +1,17 @@
 # Controller for handling replies
 from utils.twitter import get_api
 from models.tweet import Tweet
+import tweepy
 import time
+
 
 def reply_to_mentions():
     api = get_api()
-    mentions = api.mentions_timeline(since_id = Tweet.get_latest_tweet_id())
+    mentions = api.mentions_timeline(since_id=Tweet.get_latest_tweet_id())
 
-    #reversed for oldest first
+    # reversed for oldest first
     for mention in reversed(mentions):
-        if mention.in_reply_to_status_id() is not None:
+        if mention.in_reply_to_status_id is not None:
             # if not a mention to b0bot then continue
             continue
 
@@ -19,11 +21,23 @@ def reply_to_mentions():
         # the contents of the mentioned tweet
         text = mention.text
 
-        # a standard reply
-        reply_text = f"Hi @{user}, thanks for mentioning me! I'm B0Bot, a Twitter bot that provides periodic cybersecurity and hacker news to the followers of Bug Zero Twitter account."
+        # check if the mention has the #news hashtag
+        if "#news" in text:
+            # extract the keywords
+            keywords = text.split("#news ")[1].split(" ")
 
-        # reply to the mention
-        api.update_status(reply_text, in_reply_to_status_id=mention.id)
+            query = ' + '.join(keywords)
+
+            # search for tweets with the keywords
+            tweet = api.search(q=query, lang='en', result_type='recent', count=1)
+
+            # reply with the latest tweet
+            reply_text = f"Hi @{user}, here is the latest cybersecurity news on Twitter with the keywords: {', '.join(keywords)}\n"
+            
+            reply_text += f"\n{tweet.text}\n{tweet.user.name} (@{tweet.user.screen_name}): {tweet.created_at}\n"
+
+            # reply to the mention
+            api.update_status(reply_text, in_reply_to_status_id=mention.id)
 
         # to avoid rate limiting
         time.sleep(5)
